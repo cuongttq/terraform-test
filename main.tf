6ef49d53a78950663840cfb11c0fb886abcfdfbf
cuongttq <89444933+cuongttq@users.noreply.github.com>
2024-08-12 22:50:46 +0700

    Update LB

--- main.tf
+++ main.tf
@@ -55,13 +55,7 @@ resource "openstack_compute_secgroup_v2" "webapp_secgroup" {
     to_port     = 80  
     ip_protocol = "tcp"  
     cidr        = "0.0.0.0/0"  
-  }  
-  rule {  
-    from_port   = 22  
-    to_port     = 22  
-    ip_protocol = "tcp"  
-    cidr        = "0.0.0.0/0"  
-  }  
+  }
 }  
   
 resource "openstack_compute_floatingip_v2" "webapp_floatingip" {  
@@ -79,12 +73,15 @@ resource "openstack_blockstorage_volume_v3" "webapp_volume" {
   size        = 40  
   volume_type = "Premium-SSD"  
   image_id    = data.openstack_images_image_v2.ubuntu_image.id  
-}  
-  
-# Instance  
-#data "openstack_compute_keypair_v2" "webapp_key" {  
-#  name = "webapp_key"  
-#}  
+}
+
+resource "openstack_blockstorage_volume_v3" "webapp_volume_1" {  
+  name        = "webapp_volume_1"  
+  description = "Volume for webapp"  
+  size        = 40  
+  volume_type = "Premium-SSD"  
+  image_id    = data.openstack_images_image_v2.ubuntu_image.id  
+}
   
 data "openstack_compute_flavor_v2" "s2_medium_1" {  
   name = "4C4G"  
@@ -97,7 +94,8 @@ chpasswd:
   list: |    root:Welcome***123packages:  
   - nginx  - gitruncmd:  
   - systemctl enable nginx  - systemctl start nginx  - git clone https://github.com/cloudacademy/static-website-example.git  - cp -r ./static-website-example/* /var/www/html/  - rm -r ./static-website-example*/  
-  
+
+#Webapp Instance
 resource "openstack_compute_instance_v2" "webapp_instance" {  
   name            = "webapp_instance"  
   image_id        = data.openstack_images_image_v2.ubuntu_image.id  
@@ -117,19 +115,85 @@ resource "openstack_compute_instance_v2" "webapp_instance" {
     boot_index            = 0  
     delete_on_termination = true  
   }  
-}  
+}
+
+#Webapp-1 Instance
+resource "openstack_compute_instance_v2" "webapp_instance" {  
+  name            = "webapp_instance_1"  
+  image_id        = data.openstack_images_image_v2.ubuntu_image.id  
+  flavor_id       = data.openstack_compute_flavor_v2.s2_medium_1.id  
+  #key_pair        = data.openstack_compute_keypair_v2.webapp_key.name  
+  security_groups = [openstack_compute_secgroup_v2.webapp_secgroup.name]  
+  availability_zone = "nova"  
   
-resource "openstack_compute_floatingip_associate_v2" "webapp_floatingip_associate" {  
-  floating_ip = openstack_compute_floatingip_v2.webapp_floatingip.address  
-  instance_id = openstack_compute_instance_v2.webapp_instance.id  
+  user_data = "#cloud-config\npackage_update: true\nchpasswd:\n  list: |\n    root:Welcome***123\npackages:\n  - nginx\n  - git\nruncmd:\n  - systemctl enable nginx\n  - systemctl start nginx\n  - git clone https://github.com/cloudacademy/static-website-example.git\n  - cp -r ./static-website-example/* /var/www/html/\n  - rm -r ./static-website-example"  
+  network {  
+    name = openstack_networking_network_v2.webapp_network.name  
+  }  
+  block_device {  
+    uuid                  = openstack_blockstorage_volume_v3.webapp_volume_1.id  
+    source_type           = "volume"  
+    destination_type      = "volume"  
+    boot_index            = 0  
+    delete_on_termination = true  
+  }  
+}
+
+resource "openstack_lb_loadbalancer_v2" "webapp" {
+  name = "webapp"
+  vip_address = "192.168.10.100"
+  vip_subnet_id = openstack_networking_subnet_v2.webapp_subnet.id
+  flavor_id = data.openstack_compute_flavor_v2.s2_medium_1.id
+  admin_state_up = true
+}
+
+resource "openstack_lb_listener_v2" "webapp_listener" {
+  name = "webapp_listener"
+  protocol = "HTTP"
+  protocol_port = "80"
+  loadbalancer_id = openstack_lb_loadbalancer_v2.webapp.id
+}
+
+resource "openstack_lb_pool_v2" "webapp_pool" {
+  lb_method = "SOURCE_IP"
+  listener_id = openstack_lb_listener_v2.webapp_listener.id
+  admin_state_up = true
+}
+
+resource "openstack_lb_members_v2" "webapp_members" {
+  pood_id = openstack_lb_pool_v2.webapp_pool.id
+
+  member {
+    address = openstack_compute_instance_v2.webapp_instance.address
+    protocol_port = 80
+  }
+
+  member {
+    address = openstack_compute_instance_v2.webapp_instance_1.address
+    protocol_port = 80
+  }
+}
+
+resource "openstack_lb_monitor_v2" "webapp_monitor" {
+  name = "webapp_monitor"
+  pool_id = openstack_lb_pool_v2.webapp_pool.id
+  type = "PING"
+  delay = 5
+  timeout = 5
+  max_retries = 3
+}
+  
+resource "openstack_networking_floatingip_associate_v2" "webapp_floatingip_associate" {  
+  floating_ip = "103.71.97.39"
+  port_id = openstack_lb_listener_v2.webapp_listener.vip_port_id
 }
   
 output "webapp_public_ip" {  
-  value       = openstack_compute_floatingip_v2.webapp_floatingip.address  
+  value       = openstack_networking_floatingip_v2.webapp_floatingip.address  
   description = "Web Application URL"  
 }  
   
-output "webapp_private_ip" {  
-  value       = openstack_compute_instance_v2.webapp_instance.access_ip_v4  
-  description = "Web Application Private IP"  
+output "webapp_vip" {  
+  value       = openstack_lb_listener_v2.webapp_listener.vip_address  
+  description = "Web Application VIP"  
 }
