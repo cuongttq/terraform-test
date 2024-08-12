f3c98cc5b83b19c9d57f5cee0ee11147b043cc7d
ZiTechDev <45526712+zi1611061206@users.noreply.github.com>
2024-08-12 09:37:37 +0000

    test4

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ required_version = ">= 0.14.0"
 provider "openstack" {
   user_name   = "${var.user_name}"
   tenant_id = "${var.tenant_id}"
-  user_domain_name = "${var.user_domain_name}"
+  tenant_name = "BMaaS-Service"
   password    = "${var.password}"
   auth_url    = "${var.auth_url}"
   region      = "${var.region}"
